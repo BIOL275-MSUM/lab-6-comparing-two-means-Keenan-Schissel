be63bba38f2e62d2da9829b8ffb0ca8a362d8c1d
Keenan Schissel <keenan.schissel@gmail.com>
2021-04-27 10:37:20 -0500

    1st commit

---
+++ lab-6-script.R
@@ -0,0 +1,37 @@
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+
+
+# read data ---------------------------------------------------------------
+
+fish <- read_csv("chap12q19ElectricFish.csv")
+
+# put data in tidy format ------------------------------------------------
+
+fish_long <- 
+  pivot_longer(fish, speciesUpstream:speciesDownstream,
+               names_to = "location",
+               values_to = "species") %>% 
+  mutate(location = str_remove(location, c("species"))) %>% 
+  print()
+
+# do stuff ----------------------------------------------------------------
+
+fish_location_summary <-
+  fish_long %>% 
+  group_by(location) %>% 
+  summarize(
+    n = n(),
+    mean = mean(species),
+    sd = sd(species),
+    sem = sd/sqrt(n),
+    upper = mean + 1.96 * sem,
+    lower = mean - 1.96 * sem
+  ) %>% 
+  print()
+
+t.test(formula = species ~ location, data = fish_long)
+
+
